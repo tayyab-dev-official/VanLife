168e12982ef100b68cd49ad1a642caeb6706fe19
MOHAMMAD TAYYAB <tayyab.cs.usm@student.usm.my>
2025-09-02 14:46:20 +0530

    404 routes added at all levels.

--- index.jsx
+++ index.jsx
@@ -42,7 +42,9 @@ function App() {
                   <Route index element={<HostVanInfo />} />
                   <Route path="pricing" element={<HostVanPricing />} />
                   <Route path="photos" element={<HostVanPhotos />} />
+                  <Route path="*" element={<NotFound />} />
                 </Route>
+                <Route path="*" element={<NotFound />} />
               </Route>
             </Route>
 
